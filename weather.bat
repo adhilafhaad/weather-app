0c30dd4530a8100f88c1d35414e7c86a51836b38
adhilafhaad <you@example.com>
2024-10-15 21:38:50 +0530

    enjoy*2

--- weather.bat
+++ weather.bat
@@ -1,5 +1,26 @@
 @echo off
+title Weather Checker
 
-curl wttr.in/Kozhikode
+:menu
+echo Select a Region:
+echo 1. Kozhikode
+echo 2. Kochi
+echo 3. kattapana
+echo Enter your choice (1-3):
+
+set /p choice=
+
+if "%choice%"=="1" set region=Kozhikode
+if "%choice%"=="2" set region=Kochi
+if "%choice%"=="3" set region=kattapana
+
+
+if not defined region (
+    echo Invalid choice, please try again.
+    goto menu
+)
+
+echo Fetching weather information for %region%...
+curl wttr.in/%region%
 
 pause
